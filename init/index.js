4462d077f8cad8d584a0bcdf7cd93687a3fea85d
pvarunpatidar379@gmail.com <you@example.com>
2026-05-25 23:00:53 +0530

    fix new bug

--- init/index.js
+++ init/index.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then(() => {
+  .then(() => {
     console.log("connected to DB");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-   initData.data = initData.data.map((obj) => ({ ...obj, owner: "68c8474e25d375d5cf3dff16" }));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68c8474e25d375d5cf3dff16",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
 };
 
 initDB();
-
-
